0b9646534757e41500633674d305ee4c14758362
Risaro <62798663+Risaro@users.noreply.github.com>
2025-07-31 17:33:27 +0700

    Update setup-wsl.py

--- setup-wsl.py
+++ setup-wsl.py
@@ -180,7 +180,7 @@ def install_tools():
     print("🛠️ Устанавливаем htop, nano, git, curl...")
     run(["apt", "install", "-y",
          "htop", "nano", "wget", "curl", "git",
-         "software-properties-common", "gnupg2", "net-tools"])
+         "software-properties-common", "gnupg2", "net-tools","pciutils"])
     print("✅ Инструменты установлены")
 
 def install_nvidia():
